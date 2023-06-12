3d7a85cb06b490ed11cca1727de6983d1c096782
jungwon <jungwon2433@naver.com>
2023-06-12 16:54:49 +0900

    0612 props

---
+++ src/component/chap02_props/FoodList.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import FoodItem from './FoodItem'
+
+const FoodList = () => {
+  //fetch를 통해 서버에서 음식 목록을 json으로 받아온다.
+  const food = {
+    id: 11990,
+    foodList : [
+        {
+            fName: '짜장면',
+            price: 6000,
+            id: 'chinese'
+        },
+        {
+            fName: '해물파전',
+            price: 12000,
+            id: 'korean'
+        },
+        {
+            fName: '가츠동',
+            price: 8000,
+            id: 'japanese'
+        },
+        {
+            fName: '깐풍육',
+            price: 26000,
+            id: 'chinese'
+        }
+    ]
+  }
+    return (
+    <ul>
+        {/*<FoodItem foodName='짜장면' price={6000} foodId='chinese'/>
+        <FoodItem foodName='해물파전' price={12000} foodId='korean'/>
+        <FoodItem foodName='가츠동' price={8000} foodId='japanese'/>*/}     
+
+        {
+            food.foodList.map(f => <FoodItem foodName={f.fName} price={f.price} foodId={f.id}/>)
+        }
+    </ul>
+  )
+}
+
+export default FoodList
