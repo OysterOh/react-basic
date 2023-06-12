f9fe94f3e2af97a0c2e0d24b7a4570b4ba0689b7
jungwon <jungwon2433@naver.com>
2023-06-12 13:07:12 +0900

    컴포넌트 기초

---
+++ src/SayHello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//컴포넌트
+//컴포넌트 파일은 대문자로 작성하는 것이 관례이다
+function SayHello() {
+    return (
+        <div>
+            <p>hi</p>
+            <span>hello</span>
+        </div>
+    );
+}
+
+export default SayHello;
